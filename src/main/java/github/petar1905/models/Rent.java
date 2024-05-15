d27195a8ec382cfcab354cbb704a8aec11d3b860
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-05-15 15:43:08 +0300

    Add Rent constructor

--- src/main/java/github/petar1905/models/Rent.java
+++ src/main/java/github/petar1905/models/Rent.java
@@ -7,4 +7,8 @@ public class Rent {
     private @Getter User user;
     // TODO: Start date and end date
     // TODO: Setters that also change value in database.
+
+    public Rent(int userId, int mediaId) {
+        // TODO: Query database for extra info and throw exception if not found
+    }
 }
