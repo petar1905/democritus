6dd63ca1c8ad0679b1fee99612088a325fba4453
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-05-15 08:31:16 +0300

    Generate Maven project

---
+++ src/main/java/github/petar1905/App.java
@@ -0,0 +1,13 @@
+package github.petar1905;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
