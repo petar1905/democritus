61d226047b0d56df693c911883543142deb517a9
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-05-17 13:26:24 +0300

    New IO class
    The purpose of this class is to provide access to the resources mentioned in the previous commit while abstracting away many details.

---
+++ src/main/java/github/petar1905/auxillary/classes/IO.java
@@ -0,0 +1,33 @@
+package github.petar1905.auxillary.classes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class IO {
+    private static IO singleInstance = null;
+    private IO() {}
+
+    public String readFile(String path) throws IOException {
+        InputStream stream = IO.class.getClassLoader().getResourceAsStream(path);
+        InputStreamReader isr = new InputStreamReader(stream);
+        BufferedReader buffer = new BufferedReader(isr);
+        StringBuilder result = new StringBuilder();
+        String line;
+        while ((line = buffer.readLine()) != null) {
+            result.append(line+" ");
+        }
+        buffer.close();
+        isr.close();
+        stream.close();
+        return result.toString();
+    }
+
+    public static synchronized IO getInstance() {
+        if (singleInstance == null) {
+            singleInstance = new IO();
+        }
+        return singleInstance;
+    }
+}
