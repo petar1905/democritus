faac088698a858673495d85043eaea64b42053f5
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-05-20 11:24:39 +0300

    Added constructor for Rent model
    This constructor retrieves data from the database and populates the instance with it.

--- src/main/java/github/petar1905/models/Rent.java
+++ src/main/java/github/petar1905/models/Rent.java
@@ -2,13 +2,16 @@ package github.petar1905.models;
 
 import java.io.IOException;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import github.petar1905.auxillary.classes.Database;
 import github.petar1905.auxillary.classes.IO;
 import github.petar1905.auxillary.classes.Model;
 import github.petar1905.auxillary.interfaces.Deletable;
+import github.petar1905.exceptions.MediaException;
 import github.petar1905.exceptions.RentException;
+import github.petar1905.exceptions.UserException;
 import lombok.Getter;
 
 public class Rent extends Model implements Deletable {
@@ -18,9 +21,25 @@ public class Rent extends Model implements Deletable {
     private @Getter Timestamp startDate;
     private @Getter Timestamp endDate;
 
-    public Rent(int userId, int mediaId) {
+    public Rent(int id) throws SQLException, MediaException, IOException, RentException, UserException {
         deletedMsg = "This Rent instance is already deleted.";
-        // TODO: Query database for extra info and throw exception if not found
+        String queryPath = "sql/queries/database_operations/users/select_user.sql";
+        String query = IO.getInstance().readFile(queryPath);
+        Database db = Database.getInstance();
+        PreparedStatement statement = db.connection.prepareStatement(query);
+        statement.setInt(1, id);
+        ResultSet result = statement.executeQuery();
+        if (!result.next()) {
+            String format = "Rent ID %d not found.";
+            String msg = String.format(format, id);
+            throw new RentException(msg);
+        } else {
+            this.id = id;
+            this.media = new Media(result.getInt(1));
+            this.user = new User(result.getInt(2));
+            this.startDate = result.getTimestamp(3);
+            this.endDate = result.getTimestamp(4);
+        }
     }
 
     public Rent(int userId, int mediaId, Timestamp startDate, Timestamp endDate) throws SQLException, IOException {
---
+++ src/main/resources/sql/queries/database_operations/rents/select_rent.sql
@@ -0,0 +1,7 @@
+SELECT
+media_id,
+user_id,
+start_date,
+end_date
+FROM rents
+WHERE id = ?;
