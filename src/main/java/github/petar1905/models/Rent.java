9ba3e0188b61b24eb078f33bc50f92a9662eb380
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-05-20 10:04:03 +0300

    Added insert operation for Rent

--- src/main/java/github/petar1905/models/Rent.java
+++ src/main/java/github/petar1905/models/Rent.java
@@ -1,14 +1,32 @@
 package github.petar1905.models;
 
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import github.petar1905.auxillary.classes.Database;
+import github.petar1905.auxillary.classes.IO;
 import lombok.Getter;
 
 public class Rent {
     private @Getter Media media;
     private @Getter User user;
-    // TODO: Start date and end date
-    // TODO: Setters that also change value in database.
+    private @Getter Timestamp startDate;
+    private @Getter Timestamp endDate;
 
     public Rent(int userId, int mediaId) {
         // TODO: Query database for extra info and throw exception if not found
+    }
+
+    public Rent(int userId, int mediaId, Timestamp startDate, Timestamp endDate) throws SQLException, IOException {
+        String queryPath = "sql/queries/database_operations/rents/insert_rent.sql";
+        String query = IO.getInstance().readFile(queryPath);
+        Database db = Database.getInstance();
+        PreparedStatement statement = db.connection.prepareStatement(query);
+        statement.setInt(1, mediaId);
+        statement.setInt(2, userId);
+        statement.setTimestamp(3, startDate);
+        statement.setTimestamp(4, endDate);
+        statement.executeQuery();
     }
 }
--- src/main/resources/sql/queries/database_init/create_rent_table.sql
+++ src/main/resources/sql/queries/database_init/create_rent_table.sql
@@ -3,8 +3,9 @@ USE democritus;
 CREATE TABLE rents (
     media_id INT NOT NULL,
     user_id INT NOT NULL,
-    start_date DATETIME,
-    end_date DATETIME,
+    start_date TIMESTAMP,
+    end_date TIMESTAMP,
     FOREIGN KEY (media_id) REFERENCES media(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    PRIMARY KEY (media_id, user_id)
 );
