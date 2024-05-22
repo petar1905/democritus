a9cdf2d8a3d1dd9c8e32ebb745d11bc2afbd0e77
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-05-22 17:27:23 +0300

    Changed types of date fields in Rent table

--- src/main/resources/sql/queries/database_init/create_rent_table.sql
+++ src/main/resources/sql/queries/database_init/create_rent_table.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS rents (
     id INT PRIMARY KEY AUTO_INCREMENT,
     media_id INT NOT NULL,
     user_id INT NOT NULL,
-    start_date TIMESTAMP,
-    end_date TIMESTAMP,
+    start_date DATE,
+    end_date DATE,
     FOREIGN KEY (media_id) REFERENCES media(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
