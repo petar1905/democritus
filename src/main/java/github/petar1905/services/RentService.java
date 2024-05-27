1998c7fb1200e7332bf157ba74ec3e9c33487829
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-05-27 08:34:10 +0300

    Implemented getAllRents() method

--- src/main/java/github/petar1905/services/RentService.java
+++ src/main/java/github/petar1905/services/RentService.java
@@ -1,11 +1,24 @@
 package github.petar1905.services;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import github.petar1905.auxillary.classes.Database;
+import github.petar1905.auxillary.classes.IO;
+import github.petar1905.exceptions.MediaException;
+import github.petar1905.exceptions.RentException;
+import github.petar1905.exceptions.UserException;
 import github.petar1905.models.Media;
 import github.petar1905.models.Rent;
 import github.petar1905.models.User;
 
 interface RentServiceMethods {
-    Rent[] getAllRents();
+    Rent[] getAllRents() throws IOException, SQLException, MediaException, RentException, UserException;
     Rent[] getRents(User user);
     Rent[] getRents(Media media);
 }
@@ -23,9 +36,22 @@ public class RentService implements RentServiceMethods {
     }
 
     @Override
-    public Rent[] getAllRents() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllRents'");
+    public Rent[] getAllRents() throws IOException, SQLException, MediaException, RentException, UserException {
+        String queryPath = "sql/queries/database_operations/rents/select_all_rents.sql";
+        String query = IO.getInstance().readFile(queryPath);
+        Connection con = Database.getInstance().connection;
+        Statement statement = con.createStatement();
+        ResultSet rs = statement.executeQuery(query);
+        List<Rent> rentList = new ArrayList<>();
+        Rent rent;
+        while (rs.next()) {
+            rent = new Rent(rs.getInt(1));
+            rentList.add(rent);
+        }
+        Rent[] rentArray = new Rent[rentList.size()];
+        return rentList.toArray(rentArray);
+        
+
     }
 
     @Override
