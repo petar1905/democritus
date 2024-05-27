c6a191cfaebb114bbd53e0b2da01a53871d6a007
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-05-27 08:18:28 +0300

    New RentService class
    This class follows the Singleton pattern.
    The rest of the service classes are to be converted into singletons.

---
+++ src/main/java/github/petar1905/services/RentService.java
@@ -0,0 +1,42 @@
+package github.petar1905.services;
+
+import github.petar1905.models.Media;
+import github.petar1905.models.Rent;
+import github.petar1905.models.User;
+
+interface RentServiceMethods {
+    Rent[] getAllRents();
+    Rent[] getRents(User user);
+    Rent[] getRenters(Media media);
+}
+
+public class RentService implements RentServiceMethods {
+    private static RentService singleInstance = null;
+
+    private RentService() {}
+
+    public static synchronized RentService getInstance() {
+        if (singleInstance == null) {
+            singleInstance = new RentService();
+        }
+        return singleInstance;
+    }
+
+    @Override
+    public Rent[] getAllRents() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllRents'");
+    }
+
+    @Override
+    public Rent[] getRents(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRents'");
+    }
+
+    @Override
+    public Rent[] getRenters(Media media) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRenters'");
+    }
+}
