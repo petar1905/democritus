90afe83ad912e5479d3489c94436dbce275d742f
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-05-30 18:08:29 +0300

    PasswordField code cleanup

--- src/main/java/github/petar1905/views/components/settings/PasswordField.java
+++ src/main/java/github/petar1905/views/components/settings/PasswordField.java
@@ -4,6 +4,6 @@ import github.petar1905.auxillary.classes.Field;
 
 public class PasswordField extends Field {
     public PasswordField() {
-        super("Password", "password", "12345");
+        super("Password", "password", "");
     }
 }
