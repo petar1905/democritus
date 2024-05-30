6cf42da3dd78917f20dc8f4e3a2cfd2e07ef198a
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-05-30 16:31:08 +0300

    New AppProperties class
    The purpose of this class is to interact with application settings.

---
+++ src/main/java/github/petar1905/auxillary/classes/AppProperties.java
@@ -0,0 +1,34 @@
+package github.petar1905.auxillary.classes;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class AppProperties {
+    private static AppProperties singleInstance = null;
+    private String propsPath = "src/main/resources/app/app.properties";
+    private Properties props;
+
+    private AppProperties() throws IOException {
+        props = new Properties(3);
+        File propsFile = new File(propsPath);
+        if (!propsFile.exists()) propsFile.createNewFile();
+        InputStream stream = IO.class.getClassLoader().getResourceAsStream("app/app.properties");
+        props.load(stream);
+    }
+
+    public static synchronized AppProperties getInstance() throws IOException {
+        if (singleInstance == null) {
+            singleInstance = new AppProperties();
+        }
+        return singleInstance;
+    }
+
+    public void setProperty(String key, String value) throws IOException {
+        this.props.setProperty(key, value);
+        this.props.store(new FileWriter(propsPath), "hi");
+
+    }
+}
