3692aefd4880ea74083ece8cfcefaf896ed3d572
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-06-03 08:28:54 +0300

    Update View.java

--- src/main/java/github/petar1905/auxillary/classes/View.java
+++ src/main/java/github/petar1905/auxillary/classes/View.java
@@ -7,12 +7,13 @@ public class View extends JFrame {
     private View(JPanel leftPanel) {
         super();
         root = new JPanel();
-        root.setLayout(new BoxLayout(root, BoxLayout.LINE_AXIS));
+        root.setLayout(new BoxLayout(root, BoxLayout.X_AXIS));
         root.add(leftPanel);
         root.add(Box.createHorizontalGlue());
         this.add(root);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(600, 600);
+        this.setResizable(false);
     }
 
     private View(JPanel leftPanel, JPanel centerPanel) {
