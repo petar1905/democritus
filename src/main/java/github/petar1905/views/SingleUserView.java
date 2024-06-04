6918bc0f3534e62ccf04a3c5d98021a35dacd06b
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-06-04 10:03:13 +0300

    New Single User View

---
+++ src/main/java/github/petar1905/views/SingleUserView.java
@@ -0,0 +1,13 @@
+package github.petar1905.views;
+
+import github.petar1905.auxillary.classes.EmptyPanel;
+import github.petar1905.auxillary.classes.View;
+import github.petar1905.models.User;
+import github.petar1905.views.components.user_form_panel.UserForm;
+
+public class SingleUserView extends View {
+    public SingleUserView(User user) {
+        super(EmptyPanel.getInstance().panel, new UserForm(user), EmptyPanel.getInstance().panel);
+        this.setTitle(String.format("User %s", user.getName()));
+    }
+}
